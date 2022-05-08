930ef47548d90f59ffa128458735c574e8af2d6e
serkanyurttapan <serkanyurttapan34@hotmail.com>
2022-05-08 16:59:04 +0300

    added changes

---
+++ IdentityServer/IdentityServerManagement/Config.cs
@@ -0,0 +1,48 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4;
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace IdentityServerManagement
+{
+    public static class Config
+    {
+        public static IEnumerable<ApiResource> ApiResources => new ApiResource[]
+        {
+            new ApiResource ("resourse_catalog") {Scopes={"catalog_fullpermission"}},
+            new ApiResource ("resourse_photo_stock") {Scopes={"photo_stock"}},
+            new ApiResource(IdentityServerConstants.LocalApi.ScopeName)
+
+        };
+        public static IEnumerable<IdentityResource> IdentityResources =>
+                   new IdentityResource[]
+                   {
+
+                   };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            {
+                new ApiScope("catalog_fullpermission","Catalog API için full erişim"),
+                new ApiScope("photo_stock_fullpermission","Photo Stock API için full erişim"),
+                new ApiScope(IdentityServerConstants.LocalApi.ScopeName)
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+            {
+                new Client()
+                {
+                     ClientId="WebMVCClient",
+                     ClientName="Asp.Net Core MVC",
+                     ClientSecrets ={new Secret("secret".Sha256())},
+                     AllowedGrantTypes=GrantTypes.ClientCredentials,
+                    AllowedScopes={ "catalog_fullpermission", "photo_stock_fullpermission",IdentityServerConstants.LocalApi.ScopeName }
+                }
+
+            };
+    }
+}
---
+++ IdentityServer/IdentityServerManagement/Controller/UserController.cs
@@ -0,0 +1,41 @@
+﻿using IdentityServerManagement.Dtos;
+using IdentityServerManagement.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Shared.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServerManagement.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        public UserController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignupDto signupDto)
+        {
+            var user = new ApplicationUser()
+            {
+                UserName = signupDto.Username,
+                Email = signupDto.Email,
+                Country = signupDto.Country
+            };
+            var result = await _userManager.CreateAsync(user, signupDto.Password);
+            if (!result.Succeeded)
+            {
+                return BadRequest(Response<NoContent>.Fail(result.Errors.Select(x => x.Description).ToList(), 400));
+            }
+            return NoContent();
+        }
+    }
+}
